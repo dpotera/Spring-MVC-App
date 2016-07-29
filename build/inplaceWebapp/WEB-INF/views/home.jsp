326fd1475387eb84344dbf05f9c5fa9d0c66e769
dpotera <dominik.potera@gmail.com>
2016-07-30 00:52:33 +0200

    created navbar

--- build/inplaceWebapp/WEB-INF/views/home.jsp
+++ build/inplaceWebapp/WEB-INF/views/home.jsp
@@ -8,26 +8,56 @@
     <title>Spring MVC</title>
 
     <link href='https://fonts.googleapis.com/css?family=Ubuntu:400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
     <link href="<c:url value="/resources/style.css" />" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
 </head>
 <body>
 
+    <s:url value="/posts" var="postsUrl">
+        <s:param name="max" value="20"/>
+        <s:param name="number" value="10"/>
+    </s:url>
+    <s:url value="/posts" var="latestPost">
+        <s:param name="number" value="1"/>
+    </s:url>
+    <nav>
+        <div class="nav-wrapper row">
+            <div class="col s12">
+                <a href="<c:url value="/" />" class="brand-logo">Spring MVC</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="<c:url value="/user/register" />">Register</a></li>
+                    <li><a href="${postsUrl}">Posts</a></li>
+                    <li><a href="${latestPost}">Latest Post</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
     <%!
         public String createList(String[] array){
-            String result = "<ul>\n";
+            String result = "<ul class=\"collection\">\n";
             for(String li:array)
-                result += "<li>"+li+"</li>\n";
+                result += "<li class=\"collection-item\">"+li+"</li>\n";
             return result+"</ul>\n";
         }
     %>
 
-    <h1>
-        <s:message code="home.helloWorld" />
-    </h1>
-    <%! HomeController controller = new HomeController(); %>
-    <h3>Today is: <%= new Date() %></h3>
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <h1><s:message code="home.helloWorld" /></h1>
+                <%! HomeController controller = new HomeController(); %>
+                <h3>Today is: <%= new Date() %></h3>
 
-    <%= createList(new String[] {"Dominik","Adam","Name","Surname","Lorem","Ipsum"}) %>
+                <%= createList(new String[] {"Dominik","Adam","Name","Surname","Lorem","Ipsum"}) %>
+            </div>
+        </div>
+    </div>
+
 
 </body>
 </html>
--- build/serverBaseDir_jetty9/webapps-exploded/sprmvc/jsp/org/apache/jsp/WEB_002dINF/views/home_jsp.java
+++ build/serverBaseDir_jetty9/webapps-exploded/sprmvc/jsp/org/apache/jsp/WEB_002dINF/views/home_jsp.java
@@ -11,9 +11,9 @@ public final class home_jsp extends org.apache.jasper.runtime.HttpJspBase
 
 
         public String createList(String[] array){
-            String result = "<ul>\n";
+            String result = "<ul class=\"collection\">\n";
             for(String li:array)
-                result += "<li>"+li+"</li>\n";
+                result += "<li class=\"collection-item\">"+li+"</li>\n";
             return result+"</ul>\n";
         }
     
@@ -23,7 +23,9 @@ public final class home_jsp extends org.apache.jasper.runtime.HttpJspBase
   private static java.util.List<String> _jspx_dependants;
 
   private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_url_value_nobody;
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_s_param_value_name_nobody;
   private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_s_message_code_nobody;
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_s_url_var_value;
 
   private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
 
@@ -33,12 +35,16 @@ public final class home_jsp extends org.apache.jasper.runtime.HttpJspBase
 
   public void _jspInit() {
     _jspx_tagPool_c_url_value_nobody = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+    _jspx_tagPool_s_param_value_name_nobody = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
     _jspx_tagPool_s_message_code_nobody = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+    _jspx_tagPool_s_url_var_value = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
   }
 
   public void _jspDestroy() {
     _jspx_tagPool_c_url_value_nobody.release();
+    _jspx_tagPool_s_param_value_name_nobody.release();
     _jspx_tagPool_s_message_code_nobody.release();
+    _jspx_tagPool_s_url_var_value.release();
   }
 
   public void _jspService(HttpServletRequest request, HttpServletResponse response)
@@ -75,30 +81,71 @@ public final class home_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("    <title>Spring MVC</title>\n");
       out.write("\n");
       out.write("    <link href='https://fonts.googleapis.com/css?family=Ubuntu:400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>\n");
+      out.write("    <link href=\"https://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\">\n");
+      out.write("    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css\">\n");
       out.write("    <link href=\"");
       if (_jspx_meth_c_url_0(_jspx_page_context))
         return;
       out.write("\" rel=\"stylesheet\">\n");
+      out.write("\n");
+      out.write("    <script src=\"https://code.jquery.com/jquery-3.1.0.min.js\"></script>\n");
+      out.write("    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js\"></script>\n");
       out.write("</head>\n");
       out.write("<body>\n");
       out.write("\n");
       out.write("    ");
-      out.write("\n");
-      out.write("\n");
-      out.write("    <h1>\n");
-      out.write("        ");
-      if (_jspx_meth_s_message_0(_jspx_page_context))
+      if (_jspx_meth_s_url_0(_jspx_page_context))
         return;
       out.write("\n");
-      out.write("    </h1>\n");
+      out.write("    ");
+      if (_jspx_meth_s_url_1(_jspx_page_context))
+        return;
+      out.write("\n");
+      out.write("    <nav>\n");
+      out.write("        <div class=\"nav-wrapper row\">\n");
+      out.write("            <div class=\"col s12\">\n");
+      out.write("                <a href=\"");
+      if (_jspx_meth_c_url_1(_jspx_page_context))
+        return;
+      out.write("\" class=\"brand-logo\">Spring MVC</a>\n");
+      out.write("                <ul id=\"nav-mobile\" class=\"right hide-on-med-and-down\">\n");
+      out.write("                    <li><a href=\"");
+      if (_jspx_meth_c_url_2(_jspx_page_context))
+        return;
+      out.write("\">Register</a></li>\n");
+      out.write("                    <li><a href=\"");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${postsUrl}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+      out.write("\">Posts</a></li>\n");
+      out.write("                    <li><a href=\"");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${latestPost}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+      out.write("\">Latest Post</a></li>\n");
+      out.write("                </ul>\n");
+      out.write("            </div>\n");
+      out.write("        </div>\n");
+      out.write("    </nav>\n");
+      out.write("\n");
       out.write("    ");
       out.write("\n");
-      out.write("    <h3>Today is: ");
+      out.write("\n");
+      out.write("    <div class=\"container\">\n");
+      out.write("        <div class=\"row\">\n");
+      out.write("            <div class=\"col s12\">\n");
+      out.write("                <h1>");
+      if (_jspx_meth_s_message_0(_jspx_page_context))
+        return;
+      out.write("</h1>\n");
+      out.write("                ");
+      out.write("\n");
+      out.write("                <h3>Today is: ");
       out.print( new Date() );
       out.write("</h3>\n");
       out.write("\n");
-      out.write("    ");
+      out.write("                ");
       out.print( createList(new String[] {"Dominik","Adam","Name","Surname","Lorem","Ipsum"}) );
+      out.write("\n");
+      out.write("            </div>\n");
+      out.write("        </div>\n");
+      out.write("    </div>\n");
       out.write("\n");
       out.write("\n");
       out.write("</body>\n");
@@ -131,6 +178,183 @@ public final class home_jsp extends org.apache.jasper.runtime.HttpJspBase
       return true;
     }
     _jspx_tagPool_c_url_value_nobody.reuse(_jspx_th_c_url_0);
+    return false;
+  }
+
+  private boolean _jspx_meth_s_url_0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  s:url
+    org.springframework.web.servlet.tags.UrlTag _jspx_th_s_url_0 = (org.springframework.web.servlet.tags.UrlTag) _jspx_tagPool_s_url_var_value.get(org.springframework.web.servlet.tags.UrlTag.class);
+    _jspx_th_s_url_0.setPageContext(_jspx_page_context);
+    _jspx_th_s_url_0.setParent(null);
+    _jspx_th_s_url_0.setValue("/posts");
+    _jspx_th_s_url_0.setVar("postsUrl");
+    int[] _jspx_push_body_count_s_url_0 = new int[] { 0 };
+    try {
+      int _jspx_eval_s_url_0 = _jspx_th_s_url_0.doStartTag();
+      if (_jspx_eval_s_url_0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+        do {
+          out.write("\n");
+          out.write("        ");
+          if (_jspx_meth_s_param_0((javax.servlet.jsp.tagext.JspTag) _jspx_th_s_url_0, _jspx_page_context, _jspx_push_body_count_s_url_0))
+            return true;
+          out.write("\n");
+          out.write("        ");
+          if (_jspx_meth_s_param_1((javax.servlet.jsp.tagext.JspTag) _jspx_th_s_url_0, _jspx_page_context, _jspx_push_body_count_s_url_0))
+            return true;
+          out.write("\n");
+          out.write("    ");
+          int evalDoAfterBody = _jspx_th_s_url_0.doAfterBody();
+          if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+            break;
+        } while (true);
+      }
+      if (_jspx_th_s_url_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+        return true;
+      }
+    } catch (Throwable _jspx_exception) {
+      while (_jspx_push_body_count_s_url_0[0]-- > 0)
+        out = _jspx_page_context.popBody();
+      _jspx_th_s_url_0.doCatch(_jspx_exception);
+    } finally {
+      _jspx_th_s_url_0.doFinally();
+      _jspx_tagPool_s_url_var_value.reuse(_jspx_th_s_url_0);
+    }
+    return false;
+  }
+
+  private boolean _jspx_meth_s_param_0(javax.servlet.jsp.tagext.JspTag _jspx_th_s_url_0, PageContext _jspx_page_context, int[] _jspx_push_body_count_s_url_0)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  s:param
+    org.springframework.web.servlet.tags.ParamTag _jspx_th_s_param_0 = (org.springframework.web.servlet.tags.ParamTag) _jspx_tagPool_s_param_value_name_nobody.get(org.springframework.web.servlet.tags.ParamTag.class);
+    _jspx_th_s_param_0.setPageContext(_jspx_page_context);
+    _jspx_th_s_param_0.setParent((javax.servlet.jsp.tagext.Tag) _jspx_th_s_url_0);
+    _jspx_th_s_param_0.setName("max");
+    _jspx_th_s_param_0.setValue("20");
+    int _jspx_eval_s_param_0 = _jspx_th_s_param_0.doStartTag();
+    if (_jspx_th_s_param_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_tagPool_s_param_value_name_nobody.reuse(_jspx_th_s_param_0);
+      return true;
+    }
+    _jspx_tagPool_s_param_value_name_nobody.reuse(_jspx_th_s_param_0);
+    return false;
+  }
+
+  private boolean _jspx_meth_s_param_1(javax.servlet.jsp.tagext.JspTag _jspx_th_s_url_0, PageContext _jspx_page_context, int[] _jspx_push_body_count_s_url_0)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  s:param
+    org.springframework.web.servlet.tags.ParamTag _jspx_th_s_param_1 = (org.springframework.web.servlet.tags.ParamTag) _jspx_tagPool_s_param_value_name_nobody.get(org.springframework.web.servlet.tags.ParamTag.class);
+    _jspx_th_s_param_1.setPageContext(_jspx_page_context);
+    _jspx_th_s_param_1.setParent((javax.servlet.jsp.tagext.Tag) _jspx_th_s_url_0);
+    _jspx_th_s_param_1.setName("number");
+    _jspx_th_s_param_1.setValue("10");
+    int _jspx_eval_s_param_1 = _jspx_th_s_param_1.doStartTag();
+    if (_jspx_th_s_param_1.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_tagPool_s_param_value_name_nobody.reuse(_jspx_th_s_param_1);
+      return true;
+    }
+    _jspx_tagPool_s_param_value_name_nobody.reuse(_jspx_th_s_param_1);
+    return false;
+  }
+
+  private boolean _jspx_meth_s_url_1(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  s:url
+    org.springframework.web.servlet.tags.UrlTag _jspx_th_s_url_1 = (org.springframework.web.servlet.tags.UrlTag) _jspx_tagPool_s_url_var_value.get(org.springframework.web.servlet.tags.UrlTag.class);
+    _jspx_th_s_url_1.setPageContext(_jspx_page_context);
+    _jspx_th_s_url_1.setParent(null);
+    _jspx_th_s_url_1.setValue("/posts");
+    _jspx_th_s_url_1.setVar("latestPost");
+    int[] _jspx_push_body_count_s_url_1 = new int[] { 0 };
+    try {
+      int _jspx_eval_s_url_1 = _jspx_th_s_url_1.doStartTag();
+      if (_jspx_eval_s_url_1 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+        do {
+          out.write("\n");
+          out.write("        ");
+          if (_jspx_meth_s_param_2((javax.servlet.jsp.tagext.JspTag) _jspx_th_s_url_1, _jspx_page_context, _jspx_push_body_count_s_url_1))
+            return true;
+          out.write("\n");
+          out.write("    ");
+          int evalDoAfterBody = _jspx_th_s_url_1.doAfterBody();
+          if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+            break;
+        } while (true);
+      }
+      if (_jspx_th_s_url_1.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+        return true;
+      }
+    } catch (Throwable _jspx_exception) {
+      while (_jspx_push_body_count_s_url_1[0]-- > 0)
+        out = _jspx_page_context.popBody();
+      _jspx_th_s_url_1.doCatch(_jspx_exception);
+    } finally {
+      _jspx_th_s_url_1.doFinally();
+      _jspx_tagPool_s_url_var_value.reuse(_jspx_th_s_url_1);
+    }
+    return false;
+  }
+
+  private boolean _jspx_meth_s_param_2(javax.servlet.jsp.tagext.JspTag _jspx_th_s_url_1, PageContext _jspx_page_context, int[] _jspx_push_body_count_s_url_1)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  s:param
+    org.springframework.web.servlet.tags.ParamTag _jspx_th_s_param_2 = (org.springframework.web.servlet.tags.ParamTag) _jspx_tagPool_s_param_value_name_nobody.get(org.springframework.web.servlet.tags.ParamTag.class);
+    _jspx_th_s_param_2.setPageContext(_jspx_page_context);
+    _jspx_th_s_param_2.setParent((javax.servlet.jsp.tagext.Tag) _jspx_th_s_url_1);
+    _jspx_th_s_param_2.setName("number");
+    _jspx_th_s_param_2.setValue("1");
+    int _jspx_eval_s_param_2 = _jspx_th_s_param_2.doStartTag();
+    if (_jspx_th_s_param_2.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_tagPool_s_param_value_name_nobody.reuse(_jspx_th_s_param_2);
+      return true;
+    }
+    _jspx_tagPool_s_param_value_name_nobody.reuse(_jspx_th_s_param_2);
+    return false;
+  }
+
+  private boolean _jspx_meth_c_url_1(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:url
+    org.apache.taglibs.standard.tag.rt.core.UrlTag _jspx_th_c_url_1 = (org.apache.taglibs.standard.tag.rt.core.UrlTag) _jspx_tagPool_c_url_value_nobody.get(org.apache.taglibs.standard.tag.rt.core.UrlTag.class);
+    _jspx_th_c_url_1.setPageContext(_jspx_page_context);
+    _jspx_th_c_url_1.setParent(null);
+    _jspx_th_c_url_1.setValue("/");
+    int _jspx_eval_c_url_1 = _jspx_th_c_url_1.doStartTag();
+    if (_jspx_th_c_url_1.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_tagPool_c_url_value_nobody.reuse(_jspx_th_c_url_1);
+      return true;
+    }
+    _jspx_tagPool_c_url_value_nobody.reuse(_jspx_th_c_url_1);
+    return false;
+  }
+
+  private boolean _jspx_meth_c_url_2(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:url
+    org.apache.taglibs.standard.tag.rt.core.UrlTag _jspx_th_c_url_2 = (org.apache.taglibs.standard.tag.rt.core.UrlTag) _jspx_tagPool_c_url_value_nobody.get(org.apache.taglibs.standard.tag.rt.core.UrlTag.class);
+    _jspx_th_c_url_2.setPageContext(_jspx_page_context);
+    _jspx_th_c_url_2.setParent(null);
+    _jspx_th_c_url_2.setValue("/user/register");
+    int _jspx_eval_c_url_2 = _jspx_th_c_url_2.doStartTag();
+    if (_jspx_th_c_url_2.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_tagPool_c_url_value_nobody.reuse(_jspx_th_c_url_2);
+      return true;
+    }
+    _jspx_tagPool_c_url_value_nobody.reuse(_jspx_th_c_url_2);
     return false;
   }
 
